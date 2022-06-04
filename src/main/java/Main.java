f463bff4aad042aad8e7af3393bde5e72789087f
alt1983 <timofeev_a_v@mail.ru>
2022-06-04 16:39:17 +0300

    Commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-﻿import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 import ru.netology.geo.GeoService;
@@ -10,14 +10,12 @@ import ru.netology.sender.MessageSenderImpl;
 
 public class Main {
 
-    //Тестовый пример 
     public static void main(String[] args) {
         GeoService geoService = new GeoServiceImpl();
         LocalizationService localizationService = new LocalizationServiceImpl();
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.123.12.19");
         messageSender.send(headers);
     }
 }
